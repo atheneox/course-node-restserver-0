4e69e5fe6bf2a65424b852955de2d56e4af515a0
Atheneox Intruder <intruder@MBP-de-Atheneox.cpe.cableonda.net>
2020-12-27 16:26:15 -0500

    restServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 require('./config/config');
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
 
+const app = express();
+const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,36 +11,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/user', function(req, res) {
-    res.json('getUser')
-})
+app.use(require('./routes/user'));
 
-app.post('/user', function(req, res) {
-    let name = req.body.name;
-    let age = req.body.age;
+// db params
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+};
 
-    if (req.body.name === undefined) {
-        res.status(400).json({
-            ok: false,
-            msg: 'name is required'
-        });
-    } else {
-        res.json({ name, age });
-    }
-
-})
-
-app.put('/user/:id', function(req, res) {
-
-    let id = req.params.id;
-    res.json({ id });
-
-})
-
-app.delete('/user', function(req, res) {
-    res.json('deleteUser')
-})
+// db connection
+try {
+    mongoose.connect(process.env.URLDB, options);
+    console.log(`success`);
+} catch (error) {
+    handleError(error);
+    console.log(`something wrong`);
+}
 
 app.listen(process.env.PORT, () => {
+
     console.log(`listen port: ${ process.env.PORT }`);
+
 });
